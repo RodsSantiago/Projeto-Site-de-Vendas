716867025d0a0c4439848f49770c00f3ab220788
Rodrigo Santiago <rodrigosantiago30@gmail.com>
2021-11-18 11:03:43 -0300

    Exceptions created

--- backend/src/main/java/com/dscatalog/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/dscatalog/dscatalog/resources/CategoryResource.java
@@ -9,11 +9,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dscatalog.dscatalog.dto.CategoryDTO;
 import com.dscatalog.dscatalog.services.CategoryService;
+import com.dscatalog.dscatalog.services.exceptions.EntityNotFoundException;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -35,5 +37,13 @@ public class CategoryResource {
 		
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+		CategoryDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+		
+	}
+	
 
 }
--- backend/src/main/java/com/dscatalog/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/dscatalog/dscatalog/services/CategoryService.java
@@ -1,7 +1,10 @@
 package com.dscatalog.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -9,10 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 //vai acessar o repositorio e chamar no banco de dado as categorias
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dscatalog.dscatalog.dto.CategoryDTO;
 import com.dscatalog.dscatalog.entities.Category;
 import com.dscatalog.dscatalog.repositories.CategoryRepository;
+import com.dscatalog.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -20,6 +25,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll(){
 		List<Category> list = repository.findAll();
 		
@@ -37,6 +43,13 @@ public class CategoryService {
 		
 		return listDto;*/
 		
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {	
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));	
+		return new CategoryDTO(entity);
 	}
 
 }
