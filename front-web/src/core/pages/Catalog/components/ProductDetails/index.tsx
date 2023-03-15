75081537f73a0c1c552dbdb27644584553c89013
Rodrigo Santiago <rodrigosantiago30@gmail.com>
2023-03-15 15:23:18 -0300

    tela admin

--- front-web/src/core/pages/Catalog/components/ProductDetails/index.tsx
+++ front-web/src/core/pages/Catalog/components/ProductDetails/index.tsx
@@ -8,24 +8,31 @@ import { Link } from "react-router-dom";
 import { Product } from "../../../../types/Product";
 import ProductPrice from "../../../../components/ProductPrice";
 import { makeRequest } from "../../../../utils/request";
+import ProductInfoLoader from "../Loaders/ProductInfoLoader";
+import ProductDescriptionLoader from "../Loaders/ProductDescriptionLoader";
 
 type ParamsType = {
     productId: string;
 }
 
 
-const ProductDetails = () => {  
+const ProductDetails = () => {
     const { productId } = useParams<ParamsType>();
     const [product, setProduct] = useState<Product>();
+
+    //criar um estado pro loader
+    const [isLoading, setIsLoading] = useState(false);
 
     //Quando o componente iniciar buscar as infos do produto - usar UseEffect para quando iniciar 
     //como dependecia do use effect ficaria dentro do arrey - como nao tem fica o array vazio
     useEffect(() => {
+        setIsLoading(true);
         //utilizar a funcao criada makerequest para chamar o request - e passar uma URL do recurso que quer ser acessado
         //colcar como o que passamos como variavel no make request como dependencia do useEffect
-        makeRequest({url: `/products/${productId}`})
-            .then(response =>setProduct(response.data))
-    }, [productId]); 
+        makeRequest({ url: `/products/${productId}` })
+            .then(response => setProduct(response.data))
+            .finally(() => setIsLoading(false));
+    }, [productId]);
 
     return (
         <div className="product-details-container">
@@ -36,20 +43,26 @@ const ProductDetails = () => {
                 </Link>
                 <div className="row">
                     <div className="col-6 pr-5">
-                        <div className="product-details-card text-center">
-                            <img src={product?.imgUrl} alt={product?.name} className="product-details-image" />
-                        </div>
-                        <h1 className="product-details-name">
-                            {product?.name}
-                        </h1>
-                        {product?.price && <ProductPrice price={product?.price} />}
+                        {isLoading ? <ProductInfoLoader /> : (<>
+                            <div className="product-details-card text-center">
+                                <img src={product?.imgUrl} alt={product?.name} className="product-details-image" />
+                            </div>
+                            <h1 className="product-details-name">
+                                {product?.name}
+                            </h1>
+                            {product?.price && <ProductPrice price={product?.price} />}
+                        </>)}
                     </div>
 
                     <div className="col-6 product-details-card">
-                        <h1 className="product-description-title">Descricao do produto</h1>
-                        <p className="product-description-text">
-                            {product?.description}
-                        </p>
+                        {isLoading ? <ProductDescriptionLoader /> : (
+                            <>
+                                <h1 className="product-description-title">Descricao do produto</h1>
+                                <p className="product-description-text">
+                                    {product?.description}
+                                </p>
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
--- front-web/src/core/pages/Catalog/index.tsx
+++ front-web/src/core/pages/Catalog/index.tsx
@@ -5,35 +5,56 @@ import './styles.scss';
 import axios from "axios";
 import { makeRequest } from "../../utils/request";
 import { ProductResponse } from "../../types/Product";
+import ProductCardLoader from "./components/Loaders/ProductCardLoader";
+import Pagination from "core/components/Pagination";
 
 const Catalog = () => {
     //quando o componente iniciar, buscar a lista de produtos
     //quando a lista de produtos estiver disponivel popular o um estado no componente, e listar os produtos dinamicamente.
     const [productsResponse, setProductsResponse] = useState<ProductResponse>();
 
+    //criar um estado pro loader
+    const [isLoading, setIsLoading] = useState(false);
+
+    //criar um estado para deixar as cores dinamicas
+    const [activePage, setActivePage] = useState(0);
+
     //quando o componente iniciar, bucar a lista de produtos
     useEffect(() => {
         const params = {
-            page: 0,
+            page: activePage,
             linesPerPage: 12
         }
 
+        //antes de fazer a requisicao precisa iniciar o loader
+        setIsLoading(true);
         makeRequest({ url: '/products', params })
             .then(response => setProductsResponse(response.data))
-    }, []);
+            .finally(() =>{
+                //finalizar o loader
+                setIsLoading(false);
+            })
+    }, [activePage]);
 
     return (
         <div className="catalog-container">
             <h1 className="catalog-title">
                 Catálogo de produtos
-            </h1>
+            </h1>   
             <div className="catalog-products">
-                {productsResponse?.content.map(product => (
+                {isLoading ? <ProductCardLoader /> : (productsResponse?.content.map(product => (
                     <Link to={`/products/${product.id}`} key={product.id}>
                         <ProductCard product={product} />
                     </Link>
-                ))}
+                )))}
             </div>
+            {productsResponse && (
+                <Pagination 
+                    totalPages={productsResponse.totalPages}
+                    activePage={activePage}
+                    onChange={page => setActivePage(page)}
+                    />
+                 )}
         </div>
 
     );
